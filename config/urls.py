933f74abb59a9da8cd98a49b4f731785da2b18a6
Oleg Tarasov <tolegu@ya.ru>
2021-10-29 20:57:49 +0300

    Add blank start page

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import start_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', start_page),
 ]
---
+++ where_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WhereAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'where_app'
