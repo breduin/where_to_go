4c7f6a6c637f1cdcdbcf77f0eca36f69dc78e4ad
Oleg Tarasov <tolegu@ya.ru>
2021-10-31 17:31:21 +0300

    Добавлен превью для картинок в админке

--- places/admin.py
+++ places/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from places.models import Place, Image
+from django.utils.html import format_html
 
 
 admin.site.register(Image)
@@ -7,6 +8,18 @@ admin.site.register(Image)
 
 class ImageInline(admin.TabularInline):
     model = Image
+    extra = 0
+    # TODO place thumbnail column before position column
+    # fields = ('position', 'thumbnail', )
+    readonly_fields = ['thumbnail',]
+
+    def thumbnail(self, obj):
+        return format_html('<img src="{url}" height={height} />',
+            url = obj.image.url,
+            height=200
+            )
+    thumbnail.short_description = "Предпросмотр"
+
 
 
 @admin.register(Place)
